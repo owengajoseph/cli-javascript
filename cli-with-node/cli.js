9161825a6ff009dc6c19c2bc360f25357f03ec4c
owenga <owengajoseph54@gmail.com>
2025-06-11 20:54:51 +0300

    read from a file write a file

---
+++ cli-with-node/cli.js
@@ -0,0 +1,32 @@
+import { isUtf8 } from 'node:buffer';
+import { writeFileSync, readFile } from 'node:fs'
+import { join } from 'node:path';
+
+const books =
+{
+    title: "Alli goes to school",
+    genre: "Fiction",
+    type: "Children",
+    pages: 56
+};
+readFile('./data.json', 'utf8', (err, data) => {
+    if (err) {
+        console.log(err)
+    }
+    let temp = JSON.parse(data);
+    console.log(temp)
+})
+
+
+
+/* 
+{
+    let jsonData = JSON.stringify(books);
+    try {
+        writeFileSync('./data.json', jsonData, 'utf-8')
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+ */
