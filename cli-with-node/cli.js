023138dbe71ff4fc5aec2bd7a824b8a9b4377311
owenga <owengajoseph54@gmail.com>
2025-06-22 16:43:47 +0300

    use the new technique

--- cli-with-node/cli.js
+++ cli-with-node/cli.js
@@ -1,33 +1,36 @@
-import fs from 'node:fs'
-import readline from 'node:readline';
+// import fs from 'node:fs'
+import { readFileSync } from 'node:fs';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 
-let todoData;
-
-fs.readFileSync('./data.json', 'utf8', (err, data) => {
+let data = readFileSync('./data.json', 'utf8', (err, data) => {
     if (err) {
 
         console.log(err);
         return;
     }
-    todoData = JSON.parse(data)
-    console.log(todoData);
+
 })
 
-//TODOS:work on extracting data from the command line 
+console.log(data)
+
+
 function addTask(data) {
     const matches = data.match(/(["'])(?:(?=(\\?))\2.)*?\1/gi);
-    todoData.add.push(matches)
+    console.log(matches);
+    data.add.push(matches[0])
+    console.log(todoData);
     //i have pushed the data.
 }
 
-//reads data from the command line 
+//reads data from the command line
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 rl.question(`task-cli`, data => {
     if (data.includes('add')) {
-        addTask(data)
+        addTask(data);
     }
 
     if (data.includes('update')) {
@@ -67,11 +70,13 @@ rl.question(`task-cli`, data => {
 
 
     if (data.includes('list in-progress')) {
-        //TODOS:when he wants the list of the data we write then we list 
+        //TODOS:when he wants the list of the data we write then we list
         //or if there is and asynchronous function we can use that.
     }
-    console.log("please use the right commands ");
+    // console.log("please use the right commands ");
     rl.close();
+
+
 });
 
 //how im i going to handle the writing of the data.
